19a9023ea99742d2629b3acab44f9253db841983
sbhinge <sbhinge@10.0.1.236>
2014-11-21 16:46:46 +0700

    Changed to TestNG project

---
+++ BambooSeleniumProject/src/Script_SubjectLines.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+
+
+//______________________________________________________________________________________________________________________________
+//###################################                     DRIVER SCRIPT                     ####################################
+//______________________________________________________________________________________________________________________________
+//==============================================================================================================================	
+//Functions Name 								: Script (Driver)
+//Author 										: Santosh Bhigne (Santtii)	
+//Created Date 									: 5 MAY 2014
+//Parameters input/output 						: 	
+//Reviewed by 									: PJakkapong
+//Modified By 									: Santtii		
+//Modified Date									: 10 SEP 2014
+/** <B>DESCRIPTION :</B> DRIVER <P> */
+public class Script_SubjectLines {
+	@Test
+	public void ExecuteTestware() throws Exception {
+		System.out.println("Test RUN by Bamboo");
+		//TestWare execute = new TestWare(selenium, driver);
+		//execute.executeTestWare();
+	}
+}
